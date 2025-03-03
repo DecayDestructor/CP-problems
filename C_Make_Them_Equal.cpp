e2d642b77923523d42b8d34cd16756e721cd16cf
Decay_Destructor <aaryanmantri29@gmail.com>
2025-03-03 14:52:26 +0530

    new

--- C_Make_Them_Equal.cpp
+++ C_Make_Them_Equal.cpp
@@ -1,145 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define nl '\n'
-#define loop(s, n) for (ll i = s; i < n; i++)
-#define py cout << "YES"
-#define pn cout << "NO"
-#define print(ans) cout << ans << nl
-#define isEven(n) if (n % 2 == 0)
-#define ll long long
+typedef long long ll;
+const ll INF = 1e9 + 7;
+const ll MOD = 998244353;
+typedef pair<ll, ll> ii;
+#define iii pair<ll, ii>
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define pb push_back
 #define vll vector<ll>
-#define vi vector<int>
-#define vvll vector<vector<ll>>
-#define vvch vector<vector<char>>
-#define vch vector<char>
-template <typename T1, typename T2>
-using vpp = vector<pair<T1, T2>>;
-ll lcm(ll a, ll b) { return (a / __gcd(a, b)) * b; }
-// Template functions for input and output
-template <typename T>
-void inputArray(vector<T> &arr, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-}
-
-template <typename T>
-void outputArray(vector<T> &arr, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << nl;
-    }
-}
-bool RSORT(ll a, ll b)
-{
-    return a > b;
-}
-template <typename T>
-vector<T> factorization(int n)
-{
-    vector<T> factors;
-    for (int i = 1; i * i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            factors.push_back(i);
-            if (i * i != n)
-            {
-                factors.push_back(n / i);
-            }
-        }
-    }
-    return factors;
-}
-// Sieve of Eratosthenes
-vector<ll> sieveOfEratosthenes(int n)
-{
-    vector<ll> sieve(n + 1, 1);
-    sieve[0] = sieve[1] = 0; // 0 and 1 are not primes
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (sieve[i])
-        {
-            for (int j = i * i; j <= n; j += i)
-            {
-                sieve[j] = 0; // Mark multiples of i as non-prime
-            }
-        }
-    }
-    return sieve; // Return the sieve vector
-}
-// Sum of first n natural numbers
-ll sumOfNaturalNumbers(ll n)
-{
-    return (n * (n + 1)) / 2; // Formula to calculate the sum
-}
-// DFS Traversal Validation
-bool isValidDfsTraversal(ll row, ll col, ll m, ll n, vector<vll> &visited)
-{
-    return row < n && col < m && row >= 0 && col >= 0 && !visited[row][col];
-}
-// Binary Exponentiation
-ll binpow(ll a, ll b, ll m)
-{
-    a %= m;
-    ll res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
-        b >>= 1;
-    }
-    return res;
-}
-void solve()
-{
-    ll n;
-    char ch;
-    string str;
-    cin >> n >> ch >> str;
-    set<char> stt;
-    ll counter = 0;
-    for (char c : str)
-    {
-        if (c != ch)
-        {
-            counter++;
-        }
-    }
-    if (!counter)
-    {
-        cout << 0 << nl;
-        return;
-    }
-    if (str[n - 1] == ch)
-    {
-        cout << 1 << nl;
-        cout << n << nl;
-        return;
-    }
-    ll i = 1;
-    while (!(n % i))
-    {
-        i++;
-    }
-    cout << 2 << nl;
-    cout << n << " " << i << nl;
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int t = 1;
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+int main(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
     cin >> t;
-    while (t--)
-    {
-        solve();
+    while (t--) {
+        ll n;
+        cin >> n;
+        char c;
+        cin >> c;
+        string s;
+        cin >> s;
+        vector<int> ans;
+        bool ok = true;
+        for (auto x : s) {
+            if (x != c) {
+                ok = false;
+            }
+        }
+        if (!ok) {
+            f(i, 1, n + 1) {
+                ok = true;
+                f(j, i, n + 1) {
+                    ok &= (s[j - 1] == c);
+                    j += i - 1;
+                }
+                if (ok) {
+                    ans.pb(i);
+                    break;
+                }
+            }
+            if (!ok) {
+                ans.pb(n);
+                ans.pb(n - 1);
+            }
+        }
+        cout << ans.size() << "\n";
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+        cout << "\n";
     }
-    return 0;
 }
---
+++ String_Matching.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define loop(s, n) for (ll i = s; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define py cout << "YES" << nl
+#define pn cout << "NO" << nl
+#define print(ans) cout << ans << nl
+#define isEven(n) if (n % 2 == 0)
+#define ll long long
+#define vll vector<ll>
+#define vi vector<int>
+#define vvll vector<vector<ll>>
+#define vvch vector<vector<char>>
+#define vch vector<char>
+template <typename T1, typename T2>
+#define int long long
+using vpp = vector<pair<T1, T2>>;
+ll lcm(ll a, ll b) { return (a / __gcd(a, b)) * b; }
+bool RSORT(ll a, ll b) {
+    return a > b;
+}
+template <typename T>
+vector<T> factorization(int n) {
+    vector<T> factors;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            factors.push_back(i);
+            if (i * i != n) {
+                factors.push_back(n / i);
+            }
+        }
+    }
+    return factors;
+}
+// Prime Factorization
+void primeFactorisation(ll n, map<ll, ll> &mpp) {
+    for (ll i = 2; i <= sqrt(n); i++) {
+        while (n % i == 0) {
+            mpp[i]++;
+            n = n / i;
+        }
+    }
+    if (n != 1)
+        mpp[n]++;
+}
+// Sieve of Eratosthenes
+vector<ll> sieveOfEratosthenes(int n) {
+    vector<ll> sieve(n + 1, 1);
+    sieve[0] = sieve[1] = 0;  // 0 and 1 are not primes
+    for (int i = 2; i * i <= n; i++) {
+        if (sieve[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                sieve[j] = 0;  // Mark multiples of i as non-prime
+            }
+        }
+    }
+    return sieve;  // Return the sieve vector
+}
+// Sum of first n natural numbers
+ll sumOfNaturalNumbers(ll n) {
+    return (1LL * n * (n + 1)) / 2;  // Formula to calculate the sum
+}
+// DFS Traversal Validation
+bool isValidDfsTraversal(ll row, ll col, ll m, ll n, vector<vll> &visited) {
+    return row < n && col < m && row >= 0 && col >= 0 && !visited[row][col];
+}
+// Binary Exponentiation
+ll binpow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+ll mminvprime(ll a, ll m) {
+    return binpow(a, m - 2, m);
+}
+ll mod_add(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+ll mod_mul(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+ll mod_sub(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+ll mod_div(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (mod_mul(a, mminvprime(b, m), m) + m) % m;
+}
+struct Hashing {
+    string s;
+    int n;
+    int primes;
+    vector<ll> hashPrimes = {1000000009, 100000007};
+    const ll base = 31;
+    vector<vector<ll>> hashValues;
+    vector<vector<ll>> powersOfBase;
+    vector<vector<ll>> inversePowersOfBase;
+    Hashing(string a) {
+        primes = hashPrimes.size();
+        hashValues.resize(primes);
+        powersOfBase.resize(primes);
+        inversePowersOfBase.resize(primes);
+        s = a;
+        n = s.length();
+        for (int i = 0; i < hashPrimes.size(); i++) {
+            powersOfBase[i].resize(n + 1);
+            inversePowersOfBase[i].resize(n + 1);
+            powersOfBase[i][0] = 1;
+            for (int j = 1; j <= n; j++) {
+                powersOfBase[i][j] = (base * powersOfBase[i][j - 1]) % hashPrimes[i];
+            }
+            inversePowersOfBase[i][n] = mminvprime(powersOfBase[i][n], hashPrimes[i]);
+            for (int j = n - 1; j >= 0; j--) {
+                inversePowersOfBase[i][j] = mod_mul(inversePowersOfBase[i][j + 1], base, hashPrimes[i]);
+            }
+        }
+        for (int i = 0; i < hashPrimes.size(); i++) {
+            hashValues[i].resize(n);
+            for (int j = 0; j < n; j++) {
+                hashValues[i][j] = ((s[j] - 'a' + 1LL) * powersOfBase[i][j]) % hashPrimes[i];
+                hashValues[i][j] = (hashValues[i][j] + (j > 0 ? hashValues[i][j - 1] : 0LL)) % hashPrimes[i];
+            }
+        }
+    }
+    vector<ll> substringHash(int l, int r) {
+        vector<ll> hash(primes);
+        for (int i = 0; i < primes; i++) {
+            ll val1 = hashValues[i][r];
+            ll val2 = l > 0 ? hashValues[i][l - 1] : 0LL;
+            hash[i] = mod_mul(mod_sub(val1, val2, hashPrimes[i]), inversePowersOfBase[i][l], hashPrimes[i]);
+        }
+        return hash;
+    }
+};
+void solve() {
+    string str, pattern;
+    cin >> str >> pattern;
+    Hashing hash1(pattern);
+    Hashing hash2(str);
+    int requiredHash1 = hash1.substringHash(0, pattern.length() - 1)[0];
+    int requiredHash2 = hash1.substringHash(0, pattern.length() - 1)[1];
+    int l = 0, r = pattern.length() - 1;
+    int counter = 0;
+    while (r < str.length()) {
+        int foundHash1 = hash2.substringHash(l, r)[0];
+        int foundHash2 = hash2.substringHash(l, r)[1];
+        if ((foundHash1 == requiredHash1) && (foundHash2 == requiredHash2)) counter++;
+        l++, r++;
+    }
+    cout << counter << nl;
+}
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
