ab4d69496b2cf6cba19fb0515274d224a755c584
Decay_Destructor <aaryanmantri29@gmail.com>
2025-04-21 20:06:17 +0530

    hmm

---
+++ F_Expected_Median.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define loop(s, n) for (ll i = s; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define py cout << "YES" << nl
+#define pn cout << "NO" << nl
+#define print(ans) cout << ans << nl
+#define isEven(n) if (n % 2 == 0)
+#define ll long long
+#define vll vector<ll>
+#define vi vector<int>
+#define vvll vector<vector<ll>>
+#define vvch vector<vector<char>>
+#define vch vector<char>
+template <typename T1, typename T2>
+#define int long long
+using vpp = vector<pair<T1, T2>>;
+ll lcm(ll a, ll b) { return (a / __gcd(a, b)) * b; }
+bool RSORT(ll a, ll b) {
+    return a > b;
+}
+template <typename T>
+vector<T> factorization(int n) {
+    vector<T> factors;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            factors.push_back(i);
+            if (i * i != n) {
+                factors.push_back(n / i);
+            }
+        }
+    }
+    return factors;
+}
+// Prime Factorization
+map<int, int> primeFactorisation(ll n) {
+    map<int, int> mpp;
+    for (ll i = 2; i <= sqrt(n); i++) {
+        while (n % i == 0) {
+            mpp[i]++;
+            n = n / i;
+        }
+    }
+    if (n != 1)
+        mpp[n]++;
+    return mpp;
+}
+// Sieve of Eratosthenes
+vector<ll> sieveOfEratosthenes(int n) {
+    vector<ll> sieve(n + 1, 1);
+    sieve[0] = sieve[1] = 0;  // 0 and 1 are not primes
+    for (int i = 2; i * i <= n; i++) {
+        if (sieve[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                sieve[j] = 0;  // Mark multiples of i as non-prime
+            }
+        }
+    }
+    return sieve;  // Return the sieve vector
+}
+// Sum of first n natural numbers
+ll sumOfNaturalNumbers(ll n) {
+    return (1LL * n * (n + 1)) / 2;  // Formula to calculate the sum
+}
+// DFS Traversal Validation
+bool isValidDfsTraversal(ll row, ll col, ll m, ll n, vector<vll> &visited) {
+    return row < n && col < m && row >= 0 && col >= 0 && !visited[row][col];
+}
+// Binary Exponentiation
+ll binpow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+ll mminvprime(ll a, ll m) {
+    return binpow(a, m - 2, m);
+}
+ll mod_add(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+ll mod_mul(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+ll mod_sub(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+ll mod_div(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (mod_mul(a, mminvprime(b, m), m) + m) % m;
+}
+int fact[(int)2e5 + 1];
+const int MOD = 1e9 + 7;
+int inv_fact[(int)2e5 + 1];
+void calculate() {
+    fact[0] = 1, fact[1] = 1;
+    for (int i = 2; i <= 2e5; i++) {
+        fact[i] = mod_mul(i, fact[i - 1], MOD);
+    }
+    inv_fact[(int)2e5] = mminvprime(fact[(int)2e5], MOD);
+    for (int i = 2e5 - 1; i >= 0; i--) {
+        inv_fact[i] = mod_mul(inv_fact[i + 1], i + 1, MOD);
+    }
+}
+int ncr(int n, int r) {
+    if (r > n || r < 0) return 0;
+    return mod_mul(mod_mul(inv_fact[r], inv_fact[n - r], MOD), fact[n], MOD);
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    int minimum = (k + 1) / 2;
+    int answer = 0;
+    vi arr(n);
+    int counter = 0;
+    for (auto &it : arr) {
+        cin >> it;
+        counter += it;
+    }
+    int zero = n - counter;
+    for (int i = minimum; i <= min(k, counter); i++) {
+        answer += mod_mul(ncr(counter, i), ncr(zero, k - i), MOD);
+        answer %= MOD;
+    }
+    cout << answer % MOD << nl;
+}
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    calculate();
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
