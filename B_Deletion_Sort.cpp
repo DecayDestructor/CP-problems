2b72467e690ff238b87a038982338654807d2fd2
Aaryan Mantri <aaryanmantri29@gmail.com>
2026-02-28 21:16:37 +0530

    New

---
+++ B_Deletion_Sort.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define loop(s, n, inc) for (ll i = s; i < n; i += inc)
+#define all(a) a.begin(), a.end()
+#define py cout << "YES" << nl
+#define pn cout << "NO" << nl
+#define print(ans) cout << ans << nl
+#define isEven(n) if (n % 2 == 0)
+#define ll long long
+#define vll vector<ll>
+#define vi vector<int>
+#define vvll vector<vector<ll>>
+#define vvch vector<vector<char>>
+#define vvi vector<vi>
+#define pi pair<int, int>
+#define vch vector<char>
+#define int long long
+ll lcm(ll a, ll b) { return (a / __gcd(a, b)) * b; }
+bool RSORT(ll a, ll b) {
+    return a > b;
+}
+vector<int> factorization(int n) {
+    vector<int> factors;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            factors.push_back(i);
+            if (i * i != n) {
+                factors.push_back(n / i);
+            }
+        }
+    }
+    return factors;
+}
+// Prime Factorization
+map<int, int> primeFactorisation(ll n) {
+    map<int, int> mpp;
+    for (ll i = 2; i <= sqrt(n); i++) {
+        while (n % i == 0) {
+            mpp[i]++;
+            n = n / i;
+        }
+    }
+    if (n != 1)
+        mpp[n]++;
+    return mpp;
+}
+// Sieve of Eratosthenes
+vector<ll> sieveOfEratosthenes(int n) {
+    vector<ll> sieve(n + 1, 1);
+    sieve[0] = sieve[1] = 0;  // 0 and 1 are not primes
+    for (int i = 2; i * i <= n; i++) {
+        if (sieve[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                sieve[j] = 0;  // Mark multiples of i as non-prime
+            }
+        }
+    }
+    return sieve;  // Return the sieve vector
+}
+// Sum of first n natural numbers
+ll sumOfNaturalNumbers(ll n) {
+    return (1LL * n * (n + 1)) / 2;  // Formula to calculate the sum
+}
+// DFS Traversal Validation
+bool isValidDfsTraversal(ll row, ll col, ll m, ll n, vector<vll>& visited) {
+    return row < n && col < m && row >= 0 && col >= 0 && !visited[row][col];
+}
+// Binary Exponentiation
+ll binpow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+ll mminvprime(ll a, ll m) {
+    return binpow(a, m - 2, m);
+}
+ll mod_add(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+ll mod_mul(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+ll mod_sub(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+ll mod_div(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (mod_mul(a, mminvprime(b, m), m) + m) % m;
+}
+int ceil_div(int a, int b) { return (a + b - 1) / b; }
+void solve() {
+    int n;
+    cin >> n;
+    vi arr(n);
+    for (auto& it : arr) cin >> it;
+    vi temp = arr;
+    sort(all(temp));
+    if (temp == arr)
+        cout << n << nl;
+    else
+        cout << 1 << nl;
+}
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        // cout << "answer ";
+        solve();
+    }
+    return 0;
+}
